1c09e9df3a1e4c63844762fa378e731b620c6f59
ZangarKarabayev <63534481+ZangarKarabayev@users.noreply.github.com>
2022-10-22 17:40:18 +0600

    c2

---
+++ app/Http/Controllers/Api/v1/UploadImages.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Storage;
+
+class UploadImages extends Controller
+{
+    public function store(Request $request, $username){
+
+        $validator = Validator::make($request->all(), [
+            'userfile' => 'required|mimetypes:image/jpeg,image/png',
+        ]);
+
+        if ($validator->fails()) {
+            return $validator->errors()->toJson();
+        }
+
+        $path = $username . '/' . $request->file('userfile')->getClientOriginalName();
+
+        if(Storage::disk('public')->exists($path)){
+            return response('Conflict. Image exist', 409);
+        }
+
+        if($request->file('userfile')){
+            $file = $request->file('userfile');
+            $upload_folder = "public/$username";
+            $filename = $file->getClientOriginalName();
+            Storage::putFileAs($upload_folder, $file, $filename);
+            return response('file with name >> ' . $request->file('userfile')->getClientOriginalName() . ' << uploaded', 200);
+        }
+    }
+
+    public function index($username, $picture){
+
+        if(!Storage::disk('public')->exists($username . '/' . $picture)){
+            return response('Image not found', 404);
+        } 
+        
+        $file = Storage::disk('public')->get($username . '/' . $picture);  
+        return (new Response($file, 200))
+              ->header('Content-Type', 'image/jpeg');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\v1\UploadImages;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('images/{username?}/{picture?}', UploadImages::class);
+
+
